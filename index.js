6093c46261722338d45b1940684be60396dfe6c8
daniil <garipov.daniil@urfu.me>
2024-03-14 00:04:14 +0500

    Добил базу

--- index.js
+++ index.js
@@ -1,34 +1,21 @@
-
-
 require('dotenv').config()
 const express = require("express");
 const PORT = process.env.PORT
 const app = express()
-const cors = require('cors') //Импорт cors
 const fileUpload = require('express-fileupload')
+const router = require("./routers");
 const sequelize = require('./db')
-
-
-
-
-const corsOptions ={
-    origin: process.env.CLIENT,
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-
-app.use(cors(corsOptions));
+const errorHandler = require('./middlewares/ErrorHandlingMiddleware')
 
 app.use(express.json())
 app.use(fileUpload({}));
-// app.use('/api', router)
-
+app.use('/api', router)
+app.use(errorHandler)
 
 const start = async () => {
     try {
 
         await sequelize.authenticate()
-        // await sequelize.sync({alter:true})
         app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 
     } catch (e) {
